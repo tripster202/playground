a467569a58cddc0bcfaa2f3d41fbbfcb3ffc10ae
tripster202 <hockeyco28@gmail.com>
2025-04-01 22:45:42 -0600

    real py_env

--- py_env/Scripts/activate.bat
+++ py_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hocke\Desktop\playground\py_env
+set VIRTUAL_ENV=C:\Users\hocke\OneDrive\Desktop\codespace\playground\py_env
 
 if not defined PROMPT set PROMPT=$P$G
 

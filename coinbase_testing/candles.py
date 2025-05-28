a9d147f29082535ff4eab8a7d326b9f0c041f034
tripster202 <spookybooleanspoon@yahoo.com>
2025-05-27 20:04:12 -0600

    semantics

--- coinbase_testing/candles.py
+++ coinbase_testing/candles.py
@@ -3,7 +3,7 @@ import time
 
 client = RESTClient(key_file="cdp_api_key.json")
 
-def get_candles_fifteen(sym, input_hours):
+def get_candles_fifteen(symbol, input_hours):
 
     # Clamp hours between 1 and 48
     num_hours = max(min(input_hours, 48), 1)
@@ -14,7 +14,7 @@ def get_candles_fifteen(sym, input_hours):
     start = end - (num_hours * 60 * 60)
 
     return client.get_candles(
-        product_id=sym,
+        product_id=symbol,
         start=start,
         end=end,
         granularity='FIFTEEN_MINUTE'
@@ -26,6 +26,6 @@ def get_candles_fifteen(sym, input_hours):
 
 if __name__ == "__main__":
     
-    print(get_candles_fifteen(sym = 'BTC-USD', input_hours = 48))
+    print(get_candles_fifteen(symbol = 'BTC-USD', input_hours = 48))
     
     # {'candles': [{'start': '1748221800', 'low': '109421.06', 'high': '109467.28', 'open': '109442.79', 'close': '109438.28', 'volume': '2.35288209'}, {'start': '1748221500', 'low': '109369.06', 'high': '109613.58', 'open': '109613.58', 'close': '109434.61', 'volume': '27.92043725'}]}
